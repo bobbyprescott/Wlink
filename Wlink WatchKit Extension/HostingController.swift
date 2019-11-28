227dfe060ca6a48d698b9996fbe8f8ecdf687065
Bob prescott <Bobby.prescott@gmail.com>
2019-11-27 19:33:05 -0600

    Initial Commit

---
+++ Wlink WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Wlink WatchKit Extension
+//
+//  Created by robert prescott on 11/27/19.
+//  Copyright © 2019 bobcode. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Wlink WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Wlink WatchKit Extension
+//
+//  Created by robert prescott on 11/27/19.
+//  Copyright © 2019 bobcode. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
